9dabcb0f3f8741ad225ab9bffbcaf400b7b84089
Johnrobertparadiang <treborparadiang3@gmail.com>
2024-10-01 17:59:27 +0800

    place of birth

--- src/main/java/day2_java_basic_syntax/MainJavaBasics.java
+++ src/main/java/day2_java_basic_syntax/MainJavaBasics.java
@@ -3,6 +3,8 @@ package day2_java_basic_syntax;
 public class MainJavaBasics {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("JOHN ROBERT B PARADIANG");
+        System.out.println("Birth");
 
     }
 }

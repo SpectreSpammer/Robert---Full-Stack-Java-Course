5d391733bec08f450b5bd050024b36499d43dd70
SpectreSpammer <rnavarra.business@gmail.com>
2024-10-04 19:33:01 +0800

    Done with Day 2 and Day 3 Primitive

--- src/main/java/day2_java_basic_syntax/MainJavaBasics.java
+++ src/main/java/day2_java_basic_syntax/MainJavaBasics.java
@@ -1,11 +1,22 @@
 package day2_java_basic_syntax;
 
 public class MainJavaBasics {
+
     public static void main(String[] args) {
+
+
         System.out.println("Hello World");
         System.out.println("JOHN ROBERT B PARADIANG");
         System.out.println("Birth");
         System.out.println("I`m a java developer");
+
+
+        // comment - 1
+
+
+
+
+
 
     }
 }
--- src/main/java/day3_variables_data_types/primitives/MainPrimitive.java
+++ src/main/java/day3_variables_data_types/primitives/MainPrimitive.java
@@ -1,4 +1,38 @@
 package day3_variables_data_types.primitives;
 
 public class MainPrimitive {
+    public static void main(String[] args) {
+
+        //8 bit
+        byte age = 28;
+        System.out.println("byte : " + age);
+
+        //16 bit
+        short departmentCode = 101;
+        System.out.println("short : " + departmentCode);
+
+
+        char performanceGrade = 'C';
+        System.out.println("char : " + performanceGrade);
+
+        //32 bit
+        int employeeId = 1234567890;
+        System.out.println("int : " + employeeId);
+
+        float hourlyRate = 750.75237f;
+        System.out.println("float : " + hourlyRate);
+
+        //64 bit
+        double salary = 50000.75;
+        System.out.println("double : " + salary);
+
+        long sss = 1234567890987654321L;
+        System.out.println("long : " + sss);
+
+        boolean isFullTime = false;
+        System.out.println("boolean : " + isFullTime);
+
+
+
+    }
 }
